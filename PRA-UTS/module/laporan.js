a5bc7990c55957f4061a0203268456afffef2a00
anjeliNatasya <2403310038@students.satyaterrabhinneka.ac.id>
2025-11-06 01:06:05 +0700

    Update laporan.js

--- PRA-UTS/module/laporan.js
+++ PRA-UTS/module/laporan.js
@@ -1,12 +1,13 @@
-function tampilLaporan(username) {
+function tampilLaporan(username, callback) {
   const penghasilan = 2500000;
-  return `
+  const laporanHTML = `
     <div class="laporan">
       <h3>Laporan Penghasilan</h3>
       <p><strong>Admin:</strong> ${username}</p>
       <p><strong>Total Penghasilan:</strong> Rp${penghasilan.toLocaleString("id-ID")}</p>
     </div>
   `;
+  callback(laporanHTML); // jalankan callback setelah laporan siap
 }
 
 module.exports = { tampilLaporan };
