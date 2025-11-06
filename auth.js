6db8c3ccb91521f2d36c710753f52000a7c578a9
Wankaaaaaz <wawanmedan929@gmail.com>
2025-11-06 15:36:10 +0700

    Menambahkan file UTS Callback & Promise (Wankaaaaz)

---
+++ auth.js
@@ -0,0 +1,32 @@
+function cekLogin(username) {
+    return username.toLowerCase() === 'admin';
+}
+
+function generateKeyCallback(username, callback) {
+    setTimeout(() => {
+        try {
+            const key = `KEY-${Math.random().toString(36).substr(2, 9).toUpperCase()}`;
+            callback(null, key);
+        } catch (err) {
+            callback(new Error("Gagal membuat key"));
+        }
+    }, 1000);
+}
+
+function tampilkanLaporanPromise(key) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (key.startsWith('KEY-')) {
+                const penghasilan = Math.floor(Math.random() * 50000000);
+                resolve({
+                    key,
+                    penghasilan: `Rp${penghasilan.toLocaleString('id-ID')}`
+                });
+            } else {
+                reject(new Error("Key tidak valid untuk otorisasi laporan."));
+            }
+        }, 1500);
+    });
+}
+
+export { cekLogin, generateKeyCallback, tampilkanLaporanPromise };
